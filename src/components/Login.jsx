3307d33941bba6fdc9acd694e0c47f822dcb6ba1
shinewwaitun <shinewaitun28@gmail.com>
2023-10-26 14:43:54 +0630

    Added Login Form

---
+++ src/components/Login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import loginSvg from '../assets/Privacy policy-rafiki.png'
+
+function Login() {
+  return (
+    <main>
+        <div className="left container">
+            <img className="image" src={loginSvg} alt="" />
+        </div>
+        <div className="right container">
+            <form id="login-form" action="#">
+                <h2>Admin Sign In</h2>
+                <div>
+                    <label for="email">Email :</label>
+                    <input type="email" id="email"/>
+                </div>
+                <div>
+                    <label for="password">Password :</label>
+                    <input type="password" id="password"/>
+                </div>
+                <div id="remember-me-con">
+                    <input type="checkbox" id="remember-me" />
+                    <label for="remember-me">Remember me</label>
+                </div>
+                <div id="button-con">
+                    <button type="submit">Login</button>
+                </div>
+                <a href="#" id="forgot-password">Forgot password</a>
+            </form>
+        </div>
+    </main>
+  )
+}
+
+export default Login
